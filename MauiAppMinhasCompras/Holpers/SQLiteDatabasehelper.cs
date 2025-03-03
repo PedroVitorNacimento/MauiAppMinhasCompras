6a426c10f7bb0d0cbdbaf2fe160f06561654cd32
PedroVitorNacimento <p.nac124111@gmail.com>
2025-03-03 00:23:36 -0300

    construção da classe para  fazer crud no sqlite

--- MauiAppMinhasCompras/Holpers/SQLiteDatabasehelper.cs
+++ MauiAppMinhasCompras/Holpers/SQLiteDatabasehelper.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using MauiAppMinhasCompras.Models;
 
 namespace MauiAppMinhasCompras.Holpers
 {
-    internal class SQLiteDatabasehelper
+    public class SQLiteDatabasehelper
     {
+        //propriedade que armazena a conexão
+        readonly SQLiteAsyncConnection conn;
+        public SQLiteDatabasehelper(string path)
+        {
+            conn = new SQLiteAsyncConnection(path);
+            conn.CreateTableAsync<Produto>().Wait();
+        }
+        public Task<int> Insert(Produto p) 
+        {
+            return conn.InsertAsync(p);
+        }
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE produto SET Descricao=?, quantidade=?, preco=? WHERE id=?";
+            return conn.QueryAsync<Produto>(sql,p.Descricao, p.Quantidade, p.Preco, p.Id);
+        }
+        public Task<int> Delete(int id)
+        {
+           return conn.Table<Produto>().DeleteAsync(i => i.Id == id)
+        }
+
+        public Task<List<Produto>> GetAll() 
+        {
+            return conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> search(string q) 
+        {
+            string sql = "SELECT * produto WHERE descricao LIKE '%"+ q + "%'";
+            return conn.QueryAsync<Produto>(sql);
+        }
     }
 }
